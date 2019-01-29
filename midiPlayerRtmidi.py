e7d946815d4d68fbd45feac3e39cf4c18deb42f5
schef <zvanstefan@gmail.com>
2019-01-29 09:40:34 +0100

    added support for pitch instead of midi

--- midiPlayerRtmidi.py
+++ midiPlayerRtmidi.py
@@ -3,6 +3,8 @@ from threading import Timer
 from rtmidi.midiconstants import NOTE_ON, NOTE_OFF, CONTROLLER_CHANGE, ALL_NOTES_OFF
 from rtmidi.midiutil import open_midioutput
 
+import pitchParser
+
 class MidiPlayer():
     NOTE_DURATION = 1
     CLIENT_NAME = "ppt"
@@ -11,6 +13,7 @@ class MidiPlayer():
     def __init__(self):
         self.midiout_notes, self.port_name = open_midioutput(use_virtual=True, client_name=self.CLIENT_NAME,
                                                              port_name=self.PORT_NAME)
+        self.pp = pitchParser.PitchParser()
 
     def noteOn(self, note):
         # print("noteOn", note)
@@ -20,25 +23,22 @@ class MidiPlayer():
         # print("noteOff", note)
         self.midiout_notes.send_message([NOTE_OFF, note, 0])
 
-    def playSingleNote(self, note):
-        print("playSingleNote", note)
-        # self.noteOn(note)
-        # Timer(self.NOTE_DURATION, self.noteOff, [note]).start()
+    def playMultipleNotesMelodicly(self, pitchList):
+        # print("playMultipleNotesMelodicly", notes)
+        pitchListCopy = list(pitchList)
+        pitch = pitchListCopy.pop(0)
+        midi = self.pp.get_midi_from_pitch(pitch)
+        self.noteOn(midi)
+        Timer(self.NOTE_DURATION, self.noteOff, [midi]).start()
+        if len(pitchListCopy) > 0:
+            Timer(self.NOTE_DURATION, self.playMultipleNotesMelodicly, [pitchListCopy]).start()
 
-    def playMultipleNotesMelodicly(self, originalNotes):
-        print("playMultipleNotesMelodicly", originalNotes)
-        # notes = list(originalNotes)
-        # note = notes.pop(0)
-        # self.noteOn(note)
-        # Timer(self.NOTE_DURATION, self.noteOff, [note]).start()
-        # if len(notes) > 0:
-            # Timer(self.NOTE_DURATION, self.playMultipleNotesMelodicly, [notes]).start()
-
-    def playMultipleNotesHarmonicly(self, notes):
-        print("playMultipleNotesHarmonicly", notes)
-        # for note in notes:
-            # self.noteOn(note)
-            # Timer(self.NOTE_DURATION, self.noteOff, [note]).start()
+    def playMultipleNotesHarmonicly(self, pitchList):
+        # print("playMultipleNotesHarmonicly", notes)
+        for pitch in pitchList:
+            midi = self.pp.get_midi_from_pitch(pitch)
+            self.noteOn(midi)
+            Timer(self.NOTE_DURATION, self.noteOff, [midi]).start()
 
     def allNotesOff(self):
         self.midiout_notes.send_message([CONTROLLER_CHANGE, ALL_NOTES_OFF, 0])
--- pitchParser.py
+++ pitchParser.py
@@ -1,6 +1,5 @@
 import re
 
-
 class PitchParser:
     MIDI_BASE = 48
     pitchMidiBase = {
